30cfcc7b5149636788ca1970e2f738cf55753d2b
IDONTSUDO <idontsudo@gmail.com>
2024-04-15 00:34:47 +0300

    miss

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,10 @@ import "reflect-metadata";
 import { App } from "./core/controllers/app";
 import { extensions } from "./core/extensions/extensions";
 import { httpRoutes } from "./core/controllers/routes";
-import { main } from "./p";
 import { SyncMarketPlacePresentation } from "./features/sync_marketplace_products/sync_marketplace_presentation";
 import { CronController } from "./core/controllers/cron_controller";
 
 const cronProcess: CronController[] = [new SyncMarketPlacePresentation()];
 
 extensions();
-main(); 
 new App(httpRoutes, [], cronProcess).listen();
